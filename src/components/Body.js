54d577aba3f1bc20ffdb1b126d505f30338c9695
Gaurang8 <101411413+Gaurang8@users.noreply.github.com>
2023-07-02 02:24:35 +0530

    Add files via upload

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,40 @@
-import React from "react";
-import "./css/body.css";
+import React,{useRef} from "react";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import PlaylistList from "./slides/PlaylistList";
 import SongList from "./slides/SongList";
 import NewReleases from "./slides/NewReleases";
-// import Player from "./Player";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import "./css/body.css";
+import img from "./123.jpg"
+import   abc from  "./123.mp3"
+
 
 function Body() {
+
+  const audioRef = useRef(null);
+
+  const handlePlay = () => {
+      audioRef.current.src = abc;
+      audioRef.current.play();
+  };
   return (
     <div className="body-container">
+
       <NavBar />
+      
+      <div className="for-birthday">
+          <img src={img} alt="img"/>
+          <div>
+          <p className="b-text">Happy Happy Birthday </p>
+          <p className="b-text">You are one of the best things that happened to me</p>
+          <div className="play-btn-h"  onClick={() => handlePlay()}><PlayArrowIcon className="play-btn-icon-h"/></div>
+          <audio ref={audioRef} controls style={{ display: "none" }} />
+          
+          </div>
+      </div>
+
+
       <div className="slide-container">
       <SongList
           title="Bollywood Songs"
