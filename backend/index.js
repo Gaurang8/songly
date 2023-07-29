95947e7db1ad0ba91987f6604426092fe4a51b6c
gaurang <gaurang8102003@gmail.com>
2023-07-29 13:26:43 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 dotenv.config();
 app.use(express.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://sonngly.vercel.app/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
